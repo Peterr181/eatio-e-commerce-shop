2db58521a2e61c1961906e4b1adad6feeca1b078
Peterr181 <piotrzerdzinski089@gmail.com>
2024-02-04 14:57:34 +0100

    added mealdb api to categories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,22 +1,76 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Category from "./Category";
-import fish from "../../assets/fish.png";
-import vegetables from "../../assets/vege.png";
-import meat from "../../assets/meat.png";
-import groceries from "../../assets/groceries.png";
 import styles from "./Categories.module.scss";
+import useFetch from "@/hooks/useFetch";
+
+interface CategoryData {
+  categories: {
+    idCategory: string;
+    strCategoryThumb: string;
+    strCategory: string;
+  }[];
+}
 
 const Categories = () => {
+  const { data, loading, error } = useFetch<CategoryData>(
+    "https://www.themealdb.com/api/json/v1/1/categories.php"
+  );
+
+  const [currentCategoryIndex, setCurrentCategoryIndex] = useState<number>(0);
+
+  const handleMoreClick = () => {
+    if (data) {
+      setCurrentCategoryIndex((prevIndex) => {
+        if (prevIndex === data.categories.length - 4) {
+          return 0; // Reset to the beginning if at the end
+        } else {
+          return prevIndex + 1; // Increment by 1 otherwise
+        }
+      });
+    }
+  };
+
   return (
     <section className={styles.categories}>
       <div className={styles.categoriesHeader}>
         <h2>Search by Category</h2>
+        <div
+          className={styles.categoriesHeader__slide}
+          onClick={handleMoreClick}
+        >
+          <p>More</p>
+          <div className={styles.arrowIcon}>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5 12H19M19 12L12 5M19 12L12 19"
+                stroke="#131022"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
       <div className={styles.categories__items}>
-        <Category imageUrl={groceries.src} categoryName="Groceries" />
-        <Category imageUrl={fish.src} categoryName="Fish" />
-        <Category imageUrl={vegetables.src} categoryName="Vegetables" />
-        <Category imageUrl={meat.src} categoryName="Meat" />
+        {data &&
+          data.categories
+            .slice(currentCategoryIndex, currentCategoryIndex + 4)
+            .map((category) => (
+              <Category
+                key={category.idCategory}
+                imageUrl={category.strCategoryThumb}
+                categoryName={category.strCategory}
+              />
+            ))}
       </div>
     </section>
   );
